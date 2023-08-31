a6512ab43b61f8a81e624688746f4204dda50a87
Vincent Davis <vincent@heteroskedastic.net>
2023-08-30 21:56:10 -0600

    configure database

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,9 +51,10 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 DATABASES = {
     "default": env.db(
         "DATABASE_URL",
-        default="postgres:///veloteams",
+        default="postgres://postgres:postgres@localhost:5432/VeloTeams",
     ),
 }
+
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
