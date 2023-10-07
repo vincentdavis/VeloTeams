42cbcef967f5ac8afeeefd49fc7b5853a0d394e6
Vincent Davis <vincent@heteroskedastic.net>
2023-10-06 18:40:21 -0600

    change letencrypt email

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,9 +1,9 @@
 import logging
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
-from sentry_sdk.integrations.redis import RedisIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
+# from sentry_sdk.integrations.logging import LoggingIntegration
+# from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa
 from .base import env
