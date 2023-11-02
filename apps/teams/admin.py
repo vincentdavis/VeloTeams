31e081391dbcf0bb8144ad68ee0baf8bc4a067c5
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2023-11-02 23:41:42 +0530

    Added models to admin site

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Team, TeamMember
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['team_name', 'zp_id', 'zw_id', 'modified_at', 'created_at']
+    search_fields = ['team_name', 'zp_id', 'zw_id']
+    list_filter = ['modified_at', 'created_at']
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status', 'is_owner', 'is_admin', 'modified_at', 'created_at']
+    search_fields = ['user__username', 'status']
+    list_filter = ['status', 'is_owner', 'is_admin', 'modified_at', 'created_at']
+
--- apps/zp/admin.py
+++ apps/zp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import TeamRiders, TeamPending, TeamResults, Profile, ProfileVictims, ProfileSignups
 
 # Register your models here.
+@admin.register(TeamRiders)
+class TeamRidersAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
+@admin.register(TeamPending)
+class TeamPendingAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
+@admin.register(TeamResults)
+class TeamResultsAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
+@admin.register(ProfileVictims)
+class ProfileVictimsAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
+@admin.register(ProfileSignups)
+class ProfileSignupsAdmin(admin.ModelAdmin):
+    list_display = ['modified_at', 'created_at']
+
