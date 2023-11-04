6860bcbe850f03ae3f0fbb03b02a894af51c4a63
Vincent Davis <vincent@heteroskedastic.net>
2023-11-03 18:26:04 -0600

    fix migration maybe

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,19 +1,32 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from apps.zp.models import TeamRiders
+
 User = get_user_model()
 
 
 # Create your models here.
 class Team(models.Model):
     team_name = models.CharField(max_length=255, blank=False)
-    zp_id = models.IntegerField(blank=True, unique=True, verbose_name="Zwift Power ID")  # team ID at zp
-    zw_id = models.CharField(blank=True, unique=True, verbose_name="Zwift Club name")  # Club name at Zwift.com)
+    zp_id = models.IntegerField(blank=True, null=True, unique=True, verbose_name="Zwift Power ID")  # team ID at zp
+    zw_id = models.CharField(
+        blank=True, unique=True, verbose_name="Zwift Club name", default=""
+    )  # Club name at Zwift.com)
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.team_name
+
+    @property
+    def zwift_riders(self):
+        try:
+            # Get the latest TeamRiders object for this profile based on the created_at field
+            return TeamRiders.objects.filter(zp_id=self.zp_id).latest("created_at")
+        except TeamRiders.DoesNotExist:
+            # If no TeamRiders object is found, return None or handle as appropriate
+            return None
 
 
 class TeamMember(models.Model):
